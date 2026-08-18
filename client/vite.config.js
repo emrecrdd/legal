9d0a7d49424d517ce9931cd228e585007e85afff
Emre CRD <emrecirdi0@gmail.com>
2026-08-18 03:04:14 +0300

    adadadd

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+
+  build: {
+    sourcemap: false,
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: [
+            'react',
+            'react-dom',
+            'react-router-dom',
+          ],
+
+          query: [
+            '@tanstack/react-query',
+          ],
+
+          date: [
+            'dayjs',
+            'date-fns',
+          ],
+
+          icons: [
+            'lucide-react',
+          ],
+        },
+      },
+    },
+  },
+}); 
