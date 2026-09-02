8761eb0ed0ae5af812b01ea5228cb33b571d4aad
Emre CRD <emrecirdi0@gmail.com>
2026-09-02 05:40:50 +0300

    df

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import { BrowserRouter } from 'react-router-dom';
-import { QueryProvider } from './app/providers/query.provider.jsx';
-import { AuthProvider } from './app/providers/auth.provider.jsx';
-import { ThemeProvider } from './app/providers/theme.provider.jsx';
+import {
+  BrowserRouter,
+} from 'react-router-dom';
+
+import {
+  Toaster,
+} from 'react-hot-toast';
+
+import {
+  QueryProvider,
+} from './app/providers/query.provider.jsx';
+
+import {
+  AuthProvider,
+} from './app/providers/auth.provider.jsx';
+
+import {
+  ThemeProvider,
+} from './app/providers/theme.provider.jsx';
+
 import AppRouter from './app/router/index.jsx';
 
 function App() {
@@ -11,6 +27,14 @@ function App() {
         <AuthProvider>
           <ThemeProvider>
             <AppRouter />
+
+            <Toaster
+              position="top-right"
+              toastOptions={{
+                duration:
+                  3500,
+              }}
+            />
           </ThemeProvider>
         </AuthProvider>
       </QueryProvider>
